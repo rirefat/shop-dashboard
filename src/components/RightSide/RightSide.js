cedd908d063504502447509f18def518feb14017
rirefat <rirefat.official@gmail.com>
2022-09-21 18:52:51 +0600

    done customer review chart

--- src/components/RightSide/RightSide.js
+++ src/components/RightSide/RightSide.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import CustomerReview from '../CustomerReview/CustomerReview';
 import Updates from '../Updates/Updates';
 import './RightSide.css';
 
@@ -8,6 +9,7 @@ const RightSide = () => {
             <h3>Updates</h3>
             <Updates></Updates>
             <h3>Customer Review</h3>
+            <CustomerReview></CustomerReview>
         </div>
     );
 };
